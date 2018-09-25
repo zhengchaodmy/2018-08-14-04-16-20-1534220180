1f4abb9cd76c68c9b9a35a4682f6ae1cf6f0d3a2
zhengchao <15589840537@163.com>
2018-09-25 16:57:59 +0800

    max

--- main/main.js
+++ main/main.js
@@ -1,18 +1,32 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+        最大值 = ${sequence.maxmun()}
+	元素数量 = ${sequence.length()} 
+	平均值 = ${sequence.average()}
+    `);
 };
 
 class Sequence {
-  constructor(input) {
-    // Write your code here
+  constructor(sequence) {
+       this.sequence = sequence;// Write your code here
   }
 
   minimum() {
-    // Write your code here
+      return Math.min.apply(null,sequence);// Write your code here
   }
-
+  maxmum(){
+      return Math.min.apply(null,sequence);
+  }
+  length(){
+	  return sequence.length;
+  }
+  sum(x,y){
+	  return x+y;
+  }
+	  
+  average(){
+      return sequence.reduce(sum)/sequence.length;
+  }	  
   // Write your code here
 }
